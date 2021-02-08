527d76002d254b9633e98bc97e31a08545c8fd99
Awais Ahmad <awais.ahmad1990@yahoo.com>
2021-02-08 12:14:32 +0500

    replace graphql api code

--- functions/graphql/index.js
+++ functions/graphql/index.js
@@ -1,4 +1,4 @@
-const { ApolloServer, gql } = require('apollo-server-lambda');
+/*  MY CODE const { ApolloServer, gql } = require('apollo-server-lambda');
  
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
@@ -66,6 +66,76 @@ const server = new ApolloServer({
 exports.handler = server.createHandler({
   cors:{
     origin:"*",
+    credentials: true
+  }
+}); */
+const { ApolloServer, gql } = require("apollo-server-lambda");
+
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    todos: [Todo]!
+  }
+  type Todo {
+    id: ID!
+    text: String!
+    done: Boolean!
+  }
+  type Mutation {
+    addTodo(text: String!): Todo
+    updateTodoDone(id: ID!): Todo
+  }
+`;
+
+const todos = {};
+let todoIndex = 0;
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    todos: (parent, args, { user }) => {
+      if (!user) {
+        return [];
+      } else {
+        return Object.values(todos);
+      }
+    }
+  },
+  Mutation: {
+    addTodo: (_, { text }) => {
+      todoIndex++;
+      const id = `key-${todoIndex}`;
+      todos[id] = { id, text, done: false };
+      return todos[id];
+    },
+    updateTodoDone: (_, { id }) => {
+      todos[id].done = true;
+      return todos[id];
+    }
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ context }) => {
+    if (context.clientContext.user) {
+      return { user: context.clientContext.user.sub };
+    } else {
+      return {};
+    }
+  },
+  // By default, the GraphQL Playground interface and GraphQL introspection
+  // is disabled in "production" (i.e. when `process.env.NODE_ENV` is `production`).
+  //
+  // If you'd like to have GraphQL Playground and introspection enabled in production,
+  // the `playground` and `introspection` options must be set explicitly to `true`.
+  playground: true,
+  introspection: true
+});
+
+exports.handler = server.createHandler({
+  cors: {
+    origin: "*",
     credentials: true
   }
 });
